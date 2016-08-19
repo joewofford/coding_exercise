39e1a685b60996b88a3f7a978690f167686b7c73
Joseph Wofford <joewofford@Josephs-Mini.domain>
2016-08-18 22:51:45 -0600

    Almost done commenting

--- level_1f.py
+++ level_1f.py
@@ -17,18 +17,28 @@ AUTH = {'X-Starfighter-Authorization': '0c758ac77e1595c23756812113e730df324730e4
 USERNAME = 'joewofford'
 PS = 'givepeyton#18'
 PATH_TO_CHROMEDRIVER = '/Users/joewofford/anaconda/chromedriver'
-DELTA = .97
-TRADE_WINDOW = 5
+DELTA = .96
+TRADE_WINDOW = 10
 MAX_QUOTE_AGE = 1
-SPREAD_SPLIT = 1.02
+SPREAD_SPLIT = 1.03
 
 class Account(object):
 
     def __init__(self):
+        '''
+        INPUT:
+        OUTPUT:
+        Initiates an instance of the 'Account' class object, while setting the target_price attribute to 0.
+        '''
         self.target_price = 0
         return
 
     def block_buy(self, qty=100000, max_buy=1000, owned=0):
+        '''
+        INPUT:
+        OUTPUT:
+
+        '''
         self.qty = qty
         self.max_buy = max_buy
         self.owned = owned
@@ -53,9 +63,12 @@ class Account(object):
 
         return
 
-#Example quote from tickertape:
-#{"ok":true,"quote":{"symbol":"YLVO","venue":"WDBTEX","bid":5470,"ask":5497,"bidSize":5,"askSize":11446,"bidDepth":41023,"askDepth":34338,"last":5470,"lastSize":348,"lastTrade":"2016-08-18T20:56:09.856793761Z","quoteTime":"2016-08-18T20:56:09.856854129Z"}}
     def _iterative_buying(self, q):
+        '''
+        INPUT:
+        OUTPUT:
+
+        '''
         print 'Starting iterative buying...'
         while self.qty > self.owned:
             if not q.empty():
@@ -68,7 +81,6 @@ class Account(object):
 
                         #Checking if the 'current' ask price is lower than our target price
                         if ask < (self.target_price*100):
-                            print 'Price good.'
                             bid = quote['quote']['bid']
                             buy_size = min(random.randint(1, self.max_buy), (self.qty - self.owned))
 
@@ -100,14 +112,24 @@ class Account(object):
                             self.owned = self.owned + bought
                             print 'We just bought {} shares out of an intial request of {}, giving us a total of {} currently owned.'.format(str(bought), str(buy_size), str(self.owned))
 
-            time.sleep(.2)
+            time.sleep(.05)
         return
 
     def _get_tickers(self, venue):
+        '''
+        INPUT: Venue name as a string
+        OUTPUT: List of ticker symbols
+        Queries the stockfighter API to determine stocks traded on the inputted venue/exchange, and returns them as a list of ticker symbols.
+        '''
         call = requests.get('https://api.stockfighter.io/ob/api/venues/{}/stocks'.format(venue), headers=AUTH)
         return [d['symbol'] for d in call.json()['symbols']]
 
     def _get_venue(self):
+        '''
+        INPUT:
+        OUTPUT: Sets the self.venue attribute to the venue where the self.ticker stock is traded
+        Queries the stockfighter API to determine on which venue/exchange the stock denoted by the inputed ticker is traded.  Collects a list of all venues, then iterates through and examines the list of stocks on that venue.
+        '''
         print 'Getting venue now.'
         call = requests.get('https://api.stockfighter.io/ob/api/venues', headers=AUTH)
         venues = [d['venue'] for d in call.json()['venues']]
@@ -115,9 +137,17 @@ class Account(object):
         for venue in venues:
             if self.ticker in self._get_tickers(venue):
                 self.venue = venue
-                return
+        return
 
     def _single_buy(self, qty, order_type, price=0):
+        '''
+        INPUT:
+        qty - The number of shares to be bought (int)
+        order_type - The type of order (string)
+        price - The share price for the bid, if appropriate (int)
+        OUTPUT: Requests post response object (includes .json() method to access data)
+        Executes a single buy order for self.ticker on the self.account, at self.venue, purchasing the designated quantity of shares using the order type.
+        '''
         order = {
         'account': self.account,
         'venue': self.venue,
@@ -132,14 +162,29 @@ class Account(object):
         return call
 
     def _trade_status(self, t_id):
+        '''
+        INPUT: Unique (by venue) identifier of the trade
+        OUTPUT: Request response object (use .json() method to access data)
+        Queries the stockfighter API for the status of the trade corresponding to t_id.
+        '''
         call = requests.get('https://api.stockfighter.io/ob/api/venues/{}/stocks/{}/orders/{}'.format(self.venue, self.ticker, t_id), headers=AUTH)
         return call
 
     def _cancel_buy(self, t_id):
+        '''
+        INPUT: Unique (by venue) identifier of the trade
+        OUTPUT: Request response object (use .json() method to access data)
+        Queries the stockfighter API to cancel the trade corresponding to t_id.
+        '''
         call = requests.post('https://api.stockfighter.io/ob/api/venues/{}/stocks/{}/orders/{}/cancel'.format(self.venue, self.ticker, t_id), headers=AUTH)
         return call
 
     def _launch_tickertape(self, q):
+        '''
+        INPUT: Queue object
+        OUTPUT:
+        Initiates a websocket connection to the stockfighter tickertape for the self.ticker stock on the self.venue exchange.  Recieves the quote objects and stores them in a queue (after deleting the quote currently in the queue).  Thus is designed to maintain the queue with only the most recent quote in it.  Is meant to be run inside a Thread object.
+        '''
         url="wss://api.stockfighter.io/ob/api/ws/{}/venues/{}/tickertape/stocks/{}".format(self.account, self.venue, self.ticker)
         print 'Launching tickertape now.'
         while 1:
@@ -153,6 +198,11 @@ class Account(object):
         return
 
     def _login(self):
+        '''
+        INPUT:
+        OUTPUT: Selenium webdriver for the chrome browser
+        Launches a selenium webdriver and uses it to open a browser window and log into the stockfighter webpage.  It then returns the active webdriver.
+        '''
         url = 'https://www.stockfighter.io'
         b_chrome = webdriver.Chrome(executable_path = PATH_TO_CHROMEDRIVER)
         b_chrome.get(url)
@@ -163,6 +213,11 @@ class Account(object):
         return b_chrome
 
     def _initiate_market(self, b_chrome):
+        '''
+        INPUT: Selenium webdriver for the chrome browser
+        OUTPUT:
+        Uses the webdriver to navigate through the post-login stockfighter webpage to initiate a 'chock_a_block' game.
+        '''
         b_chrome.find_element_by_xpath('//*[@id="app"]/div/div/div/div/div[1]/div[2]/ul/li[1]/b/a').click()
         time.sleep(3)
         b_chrome.find_element_by_xpath('//*[@id="wrapping"]/nav/div/ul/li[2]').click()
@@ -172,6 +227,11 @@ class Account(object):
         return
 
     def _parse_trade_info(self, b_chrome):
+        '''
+        INPUT: Selenium webdriver for the chrome browser
+        OUTPUT:
+        Uses the webdriver to parse the opening page of the 'chock_a_block' game for the account to be traded on (self.account), and the stock to be traded (self.ticker). It then closes the pop-up window.
+        '''
         self.account = b_chrome.find_element_by_xpath('/html/body/div[3]/div/div[2]/div/div/div[2]/span/p[2]/strong[2]').text.split()[1]
         self.ticker = b_chrome.find_element_by_xpath('/html/body/div[3]/div/div[2]/div/div/div[2]/span/p[2]/em').text
         time.sleep(2)
@@ -179,6 +239,11 @@ class Account(object):
         return
 
     def _parse_target_price(self, b_chrome):
+        '''
+        INPUT: Selenium webdriver for the chrome browser
+        OUTPUT: Boolean
+        Uses the webdriver to parse the 'chock_a_block' game page for the 'customer target price'. If it is able to find the target price it returns true (will only succeed once the pop-up window with the information appears), otherwise returns false.
+        '''
         temp = b_chrome.find_elements_by_xpath('//*[@id="wrapping"]/div/div[1]/div/div[1]/p')
         if len(temp) == 0:
             return False
@@ -187,11 +252,21 @@ class Account(object):
             return True
 
     def _sum_fills(self, trade):
+        '''
+        INPUT: Requests return item corresponding to a trade status query
+        OUTPUT: The total number of shares purchased thus far (int)
+        Aggregates all of the fills associated with the trade, resulting in the total number of shares bought at the time of query.
+        '''
         return sum([x['qty'] for x in trade.json()['fills']])
 
     def _extract_target_price(self, b_chrome):
+        '''
+        INPUT: Selenium webdriver for the chrome browser
+        OUTPUT:
+        Initiates small trades to instigate the stockfighter website to open the pop-up window containing the customer target price, then parse this price using the webdriver and store it as the self.target_price attribute.
+        '''
         print 'Starting to try to extract target price...'
-        #Make small buys to get the target price to pop up, and checking if its there yet, parse and store as attribute, then
+        #Make small buys to get the target price to pop up, and checking if its there yet, parse and store as attribute
         while self.target_price == 0:
             buy = self._single_buy(5, 'market')
             while buy.status_code != requests.codes.ok:
@@ -199,7 +274,6 @@ class Account(object):
             t_sent = time.time()
             t_id = buy.json()['id']
             bought = self._sum_fills(self._trade_status(t_id))
-            print 'Bought = {}'.format(str(bought))
 
             #Checking if the full order was filled, and monitoring status if not until the trade has been open for TRADE_WINDOW, then close and add the number of shares purchased to the class attribute
             while bought < 5:
@@ -210,7 +284,6 @@ class Account(object):
                     break
                 time.sleep(.1)
                 bought = self._sum_fills(self._trade_status(t_id))
-                print 'Bought = {}'.format(str(bought))
 
             self.owned = self.owned + bought
             print 'We just bought {} shares out of an intial request of {}, giving us a total of {} currently owned.'.format(str(bought), '5', str(self.owned))
