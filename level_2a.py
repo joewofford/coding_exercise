adc32d625a528d3a9fa92ac91398841a9f1208f5
Joseph Wofford <joewofford@Josephs-Mac-mini.local>
2016-08-19 14:04:56 -0600

    moving functionality over from level 1 to apply to level 2, time to write the actual market making algorythm

---
+++ level_2a.py
@@ -0,0 +1,146 @@
+from __future__ import division
+
+import requests
+import random
+import time
+import websocket
+import json
+import Queue
+
+from threading import Thread
+from selenium import webdriver
+from datetime import datetime
+
+#API key for the stockfighter website
+AUTH = {'X-Starfighter-Authorization': '0c758ac77e1595c23756812113e730df324730e4'}
+
+#User information for login (user specific)
+USERNAME = 'joewofford'
+PS = 'givepeyton#18'
+
+#path to the chromedriver for selenium to use (machine specific)
+PATH_TO_CHROMEDRIVER = '/Users/joewofford/anaconda/chromedriver'
+
+#Parameters related to how the market-making will be executed
+#How deep into the spread we will big/ask (larger number is more aggressive)
+TRADE_AGGRESSION = .03
+#How long a trade will be left active (seconds)
+TRADE_WINDOW = 10
+#The maximum age, in seconds, of a quote to use its information to initiate a trade
+MAX_QUOTE_AGE = 1
+
+
+
+class MakeMarket(object):
+
+    def __init__(self):
+        return
+
+    def _single_trade(self, qty, direction, price, order_type='limit'):
+        '''
+        INPUT:
+        qty - The number of shares to be bought/sold (int)
+        direction - Whether the trade is a buy or sell (string)
+        price - The price to bid/ask for the trade (int)
+        order_type - The type of order to initiate, defaults to 'limit' (string)
+        OUTPUT: Resests post response object (includes .json() method ot access data)
+        Executes a single trade order for self.ticker on the self.account, at self.venue, initiating a trade for the designated quantity of shares using the order type.
+        '''
+        order = {
+        'account': self.account,
+        'venue': self.venue,
+        'stock': self.ticker,
+        'qty': qty,
+        'direction': direction,
+        'price': price,
+        'orderType': order_type
+        }
+
+        call = requests.post('https://api.stockfighter.iio/ob/api/venues/{}/stocks/{}/orders'.format(self.venue, self.ticker), headers=AUTH, json=order)
+        return call
+
+    def _trade_status(self, t_id):
+        '''
+        INPUT: Unique (by venue) identifier of the trade
+        OUTPUT: Request response object (use .json() method to access data)
+        Queries the stockfighter API for the status of the trade corresponding to t_id.
+        '''
+        call = requests.get('https://stockfighter.io/ob/api/venues/{}/stocks/{}/orders/{}'.format(self.venue, self.ticker, t_id), headers=AUTH)
+        return call
+
+    def _cancel_trade(self, t_id):
+        '''
+        INPUT: Unique (by venue) identifier of the trade
+        OUTPUT: Request response object (use .json() method to access data)
+        Queries the stockfighter API to cancel the trade corresponding to t_id.
+        '''
+        call = requests.post('https://api.sockfighter.io/ob/api/venues/{}/stocks/{}/orders/{}/cancel'.format(self.venue, self.ticker, t_id), headers=AUTH)
+        return call
+
+    def _launch_tickertape(self, q):
+        '''
+        INPUT: Queue object
+        OUTPUT:
+        Initiates a websocket connection to the stockfighter tickertape for the self.ticker stock on the self.venue exchange.  Recieves the quote objects and stores them in a queue (after deleting the quote currently in the queue).  Thus is designed to maintain the queue with only the most recent quote in it.  It is meant to be run inside a Thread object.
+        '''
+        url = 'wss://api.stockfighter.io/ob/api/ws/{}/venues/{}/tickertape/stocks/{}'.format(self.account, self.venue, self.ticker)
+        print 'Launching tickertape now.'
+        while 1:
+            try:
+                tick = ws.recv()
+                with q.mutex:
+                    q.queue.clear()
+                q.put(tick)
+            except:
+            ws = websocket.create_connection(url)
+        return
+
+    def _login(self):
+        '''
+        INPUT:
+        OUTPUT: Selenium webdriver for the chrome browser
+        Launches a selenium webdriver and uses it to open a browser window and log into the stockfighter webpage.  It then returns the active webdriver.
+        '''
+        url = 'https://www.stockfighter.io'
+        b_chrome = webdriver.Chrome(executable_path = PATH_TO_CHROMEDRIVER)
+        b_chrome.get(url)
+        b_chrome.find_element_by_name('session[username]').send_keys(USERNAME)
+        b_chrome.find_element_by_name('session[password]').send_keys(PS)
+        b_chrome.find_element_by_xpath('//*[@id="loginform"]/button').click()
+        time.sleep(10)
+        return b_chrome
+
+    def _initiate_market(self, b_chrome):
+        '''
+        INPUT: Selenium webdriver for the chrome browser
+        OUTPUT:
+        Uses the webdriver to navigate through the post-login stockfighter webpage to initiate a 'sell_side' game.
+        '''
+        b_chrome.find_element_by_xpath('//*[@id="app"]/div/div/div/div/div[1]/div[2]/ul/li[1]/b/a').click()
+        time.sleep(3)
+        b_chrome.find_element_by_xpath('//*[@id="wrapping"]/nav/div/ul/li[2]').click()
+        time.sleep(1)
+        b_chrome.find_element_by_xpath('//*[@id="wrapping"]/nav/div/ul/li[2]/ul/li[2]/a/span[1]/b').click()
+        time.sleep(10)
+        return
+
+    def _parse_trade_info(self, b_chrome):
+        '''
+        INPUT: Selenium webdriver for the chrome browser
+        OUTPUT:
+        Uses the webdriver to parse the opening page of the 'sell_side' game for the account to be traded on (self.account), the exchange where the trades will take place (self.venue), and the stock to be traded (self.ticker).  It then closes the pop-up window.
+        '''
+        self.account = p_chrome.find_element_by_xpath('/html/body/div[3]/div/div[2]/div/div/div[2]/span/p[1]/strong').text.split()[1]
+        self.venue = p_chrome.find_element_by_xpath('/html/body/div[3]/div/div[2]/div/div/div[2]/span/p[1]/em[1]').text
+        self.ticker = p_chrome.find_element_by_xpath('/html/body/div[3]/div/div[2]/div/div/div[2]/span/p[1]/em[2]').text
+        time.sleep(2)
+        b_chrome.find_element_by_xpath('/html/body/div[3]/div/div[2]/div/div/div[3]/button').click()ßßß
+        return
+
+    def _sum_fills(self, trade):
+        '''
+        INPUT: Requests return item corresponding to a trade status query
+        OUTPUT: The total number of shares filled (bought or sold) thus far (int)
+        Aggregataes all of the fills associated with the trade, resulting in the total number of shares bought/sold at the time of query.
+        '''
+        return sum([x['qty'] for x in trade.json()['fills']])
